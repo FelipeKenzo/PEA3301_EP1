f3ca38b53f9e4611c47b6d834cb72140fa0adf69
FelipeKenzo <felipe.kenzo@live.com>
2019-06-06 13:55:45 -0300

    matrizes Ypr e Q

--- main_program.m
+++ main_program.m
@@ -5,23 +5,27 @@ VOL069;
 
   %-----Montagem da matriz de admitâncias primitivas-----%
   distancia = 0;
-  Ypr = zeros(rows(topologia) , rows(topologia));
+  Ypr = zeros(3*rows(topologia) , 3*rows(topologia));
   
+  k=1;
   for( i=1:rows(topologia))
+     k=3*i-2;
      distancia = topologia(i, 3);
      
-     % matrizAd = | Ya Yab   Yca |
-     %            |Yab  Yb   Ybc |
-     %            |Yca  Ybc   Yc |
+     % matrizAd = | Ya   0   0 |
+     %            |  0  Yb   0 |
+     %            |  0   0  Yc |
      
-     matrizAd = [ 1 / (topologia(i, 4) + i*topologia(i, 5)) , 1 / (topologia(i, 6) + i*topologia(i, 7)) , 1 / (topologia(i, 8) + i*topologia(i, 9))
-                  1 / (topologia(i,10) + i*topologia(i,11)) , 1 / (topologia(i,12) + i*topologia(i,13)) , 1 / (topologia(i,14) + i*topologia(i,15))
+     matrizAd = [ 1 / (topologia(i, 4) + j*topologia(i, 5)) , 1 / (topologia(i, 6) + j*topologia(i, 7)) , 1 / (topologia(i, 8) + j*topologia(i, 9))
+                  1 / (topologia(i,10) + j*topologia(i,11)) , 1 / (topologia(i,12) + j*topologia(i,13)) , 1 / (topologia(i,14) + j*topologia(i,15))
+                  1 / (topologia(i,16) + j*topologia(i,17)) , 1 / (topologia(i,18) + j*topologia(i,19)) , 1 / (topologia(i,20) + j*topologia(i,21))
                 ];
                 
      matrizAd = (1 / distancia) * matrizAd;
      
      %Não existem mútuas entre os ramos
-     Ypr(i,i) = matrizAd;     
+     Ypr(k:k+2,k:k+2) = matrizAd;  
+        
   end;
   
   %-----Montagem da matriz de incidências Q-----%
@@ -29,24 +33,35 @@ VOL069;
   countNos = 35;
   countRamos = rows(topologia);
   um = eye(3);
-  zero = zeroes(3);
+  zero = zeros(3);
   
-  Q = zeroes(countRamos, countNos);
+  Q = zeros(3*countRamos, 3*countNos);
   
-  for(i = 1:countRamos)  
-    Q( i , topologia(i,1)/10) = um;
-    Q( i , topologia(i,2)/10) = -1 * um;  
-  end;
+  k=1;
+  for(i=1 : countRamos)
+    k = 3*i-2;
+    p = topologia(i,1)/10; %pai (barra inicial)
+    f = topologia(i,2)/10; %filho (barra final)
   
-  for(i = 1:countRamos)
-    for(j = 1:countNos)
-      if((Q(i,j) != um ) || Q(i,j) != -1 * um)
-        Q(i,j) = zero;
+    Q( k:k+2 , 3*p-2:3*p) = um;
+    Q( k:k+2 , 3*f-2:3*f) = -1 * um;
+    
+  end;  
+
+  
+  for(i = 1:3:3*countRamos)
+    for(j = 1:3:3*countNos)
+      if((Q(i:i+2,j:j+2) != um ) || Q(i:i+2,j:j+2) != -1 * um)
+        Q(i:i+2,j:j+2) = zero;
       end;
     end;
   end;
-  
  
+ 
+ Ybus=transpose(Q)*Ypr*Q;
+ 
+ Yprf = zeros( 3*(rows(topologia)+1) , 3*(rows(topologia)+1) );
+ Qf = zeros( 3*(countRamos+1) , 3*(countNos+1) );
   
   
 
