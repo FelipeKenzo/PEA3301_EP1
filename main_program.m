e81ecb3ad20ea2654cf394824e9e3097b02f704d
FelipeKenzo <felipe.kenzo@live.com>
2019-06-06 11:38:49 -0300

    -a

--- main_program.m
+++ main_program.m
@@ -1,18 +1,21 @@
-function [retval] = main_program (TOP, CAR, VOL)
-  
-  %-----Montagem da matriz de admitancias primitivas-----%
+
+TOP069;
+CAR069;
+VOL069;
+
+  %-----Montagem da matriz de admitâncias primitivas-----%
   distancia = 0;
-  Ypr = zeros(rows(TOP) , rows(TOP));
+  Ypr = zeros(rows(topologia) , rows(topologia));
   
-  for( i=1:rows(TOP))
-     distancia = TOP(i, 3);
+  for( i=1:rows(topologia))
+     distancia = topologia(i, 3);
      
      % matrizAd = | Ya Yab   Yca |
      %            |Yab  Yb   Ybc |
      %            |Yca  Ybc   Yc |
      
-     matrizAd = [ 1 / (TOP(i, 4) + i*TOP(i, 5)) , 1 / (TOP(i, 6) + i*TOP(i, 7)) , 1 / (TOP(i, 8) + i*TOP(i, 9))
-                  1 / (TOP(i,10) + i*TOP(i,11)) , 1 / (TOP(i,12) + i*TOP(i,13)) , 1 / (TOP(i,14) + i*TOP(i,15))
+     matrizAd = [ 1 / (topologia(i, 4) + i*topologia(i, 5)) , 1 / (topologia(i, 6) + i*topologia(i, 7)) , 1 / (topologia(i, 8) + i*topologia(i, 9))
+                  1 / (topologia(i,10) + i*topologia(i,11)) , 1 / (topologia(i,12) + i*topologia(i,13)) , 1 / (topologia(i,14) + i*topologia(i,15))
                 ];
                 
      matrizAd = (1 / distancia) * matrizAd;
@@ -24,15 +27,15 @@ function [retval] = main_program (TOP, CAR, VOL)
   %-----Montagem da matriz de incidências Q-----%
   
   countNos = 35;
-  countRamos = rows(TOP);
+  countRamos = rows(topologia);
   um = eye(3);
   zero = zeroes(3);
   
   Q = zeroes(countRamos, countNos);
   
   for(i = 1:countRamos)  
-    Q( i , TOP(i,1)/10) = um;
-    Q( i , TOP(i,2)/10) = -1 * um;  
+    Q( i , topologia(i,1)/10) = um;
+    Q( i , topologia(i,2)/10) = -1 * um;  
   end;
   
   for(i = 1:countRamos)
@@ -47,4 +50,4 @@ function [retval] = main_program (TOP, CAR, VOL)
   
   
 
-endfunction
+%%endfunction
